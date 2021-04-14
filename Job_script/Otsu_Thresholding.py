6ea5fa9dd21f8b538151dd958435625de7af2cf0
Jamie McGregor <jamie.mcgregor@postgrad.manchester.ac.uk>
2021-04-14 17:49:10 +0100

    Changed file type of functions from ipynb to py for ease

---
+++ Job_script/Otsu_Thresholding.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Import any necessary modules as required
+
+# In[ ]:
+
+
+import sys
+import numpy as np
+import skimage.color
+import skimage.filters
+import skimage.io
+import skimage.viewer
+
+
+# First the file data has to be imported from the initial processing
+
+# In[ ]:
+
+
+def Otsu(ImgProcessed)
+    In = skimage.io.imread(ImgProcessed)
+
