e6f841de94ae9754f57b25b4e20317d18445245f
Jamie McGregor <jamie.mcgregor@postgrad.manchester.ac.uk>
2021-04-15 11:17:47 +0100

    Uploading work I have completed so far in relation to previous commits

--- Job_script/Otsu_Thresholding.py
+++ Job_script/Otsu_Thresholding.py
@@ -2,23 +2,21 @@
 # coding: utf-8
 
 # Import any necessary modules as required
+import cv2
 
-# In[ ]:
-
-
-import sys
-import numpy as np
-import skimage.color
-import skimage.filters
-import skimage.io
-import skimage.viewer
-
+Test = cv2.imread('Chu10.JPG',0)
 
 # First the file data has to be imported from the initial processing
+def Otsu(ImgIn):
+    
+# Use a Gaussian blurring technique on the image to reduce noise - the area for blurring was chosen as (3,3) to maintain accuracy
+    Blur = cv2.GaussianBlur(ImgIn, (3,3),0)
+    
+# Use adaptive Otsu method for gaining threshold value
+    ThreshValue, ThreshImg = cv2.threshold(Blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    
+    return ThreshImg
 
-# In[ ]:
-
-
-def Otsu(ImgProcessed)
-    In = skimage.io.imread(ImgProcessed)
-
+# To transfer the data into a 2D image matplotlib is required as shown:
+#   from matplotlib import pyplot as plt
+#   plt.imshow(ThreshImg, 'gray')
