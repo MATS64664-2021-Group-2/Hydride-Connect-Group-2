49e9562def04af6bf4e34148c99612be17596336
Wunmi Olukoya <w.t.olukoya@gmail.com>
2021-05-28 15:43:27 +0100

    clean up repository

--- Job_script/README.md
+++
@@ -1,3 +0,0 @@
-# Job_script
-
-This is a bit of Python code is to load the images directly from github
--- Job_script/packages/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr 15 11:31:06 2021
-
-@author: a77510jm
-"""
-
