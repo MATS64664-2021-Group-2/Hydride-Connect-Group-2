6b517eab607a8a100bb00b05412ee8085c55660c
Wunmi Olukoya <w.t.olukoya@gmail.com>
2021-05-28 15:28:12 +0100

    changing processing_test

--- Job_script/tests/processing_test.py
+++ Job_script/tests/processing_test.py
@@ -7,27 +7,26 @@ import pytest
 from matplotlib import pyplot as plt
 import re
 from PIL import Image
-import os, os.path
+#import os, os.path
+import sys
 from os import listdir
 import matplotlib.axes
 import matplotlib.image as mpimg
 import numpy as np
 import cv2
 
+#absolute path to file
+sys.path.append("Hydride-Connect-Group-2/Job_script/packages")
 
-# import the package
-#import packages
+# import the package modules 
+import loading
 
-# import the antigravity module
-from ../packages import loading
-
-# import the antigravity module
-from ../packages import processing
+import processing
 
 #pre processing
 
-mypath = '/base_line_images/'
-def_files = loading.sorted_aphanumeric(listdir(mypath))
+#mypath = '/base_line_images/'
+#def_files = loading.sorted_aphanumeric(listdir(mypath))
 
 # The data of the image that is going to be analysed are storaged in the "img" variable
 img_1 = cv2.imread('/base_line_images/45Degrees.png',0)
