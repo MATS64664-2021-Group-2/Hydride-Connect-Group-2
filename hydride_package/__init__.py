b0a8596afecdfd8b7497a975dae30cd99662c1f7
Jamie McGregor <jamie.mcgregor@postgrad.manchester.ac.uk>
2021-06-11 14:20:32 +0100

    removing unnecessary folders

--- hydride_package/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Apr 15 11:31:06 2021
-
-@author: a77510jm
-"""
-
-__all__ = ["connectivity", "edge", "loading", "parameters" , "plot_image", "processing", "image_count", "image_clean_up", "Gaussian_thresholding"]
