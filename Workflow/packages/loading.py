b97adb0a6060774dc06a6337425b6271650cf360
Jamie McGregor <jamie.mcgregor@postgrad.manchester.ac.uk>
2021-06-11 16:25:49 +0100

    Made directory creation Robust and removed unnecessary directory

--- Workflow/packages/loading.py
+++ Workflow/packages/loading.py
@@ -2,6 +2,8 @@
 
 import os.path
 from os import path
+import shutil
+import stat
 
 import re
 
@@ -30,23 +32,32 @@ def DirCheck():
     
     if path.exists("ImageStates") is False:
         os.mkdir("ImageStates")
-    
-    if path.exists("ImageStates/Connectivity") is False:
-        os.mkdir("ImageStates/Connectivity")
         
     if path.exists("ImageStates/Edges") is False:
         os.mkdir("ImageStates/Edges")
         
     if path.exists("ImageStates/Edges/ContKmeans") is False:
         os.mkdir("ImageStates/Edges/ContKmeans")
+    else: 
+        shutil.rmtree("ImageStates/Edges/ContKmeans", onerror=remove_readonly)
+        os.mkdir("ImageStates/Edges/ContKmeans")
         
     if  path.exists("ImageStates/Edges/ContOtsu") is False:
+        os.mkdir("ImageStates/Edges/ContOtsu")
+    else: 
+        shutil.rmtree("ImageStates/Edges/ContOtsu", onerror=remove_readonly)
         os.mkdir("ImageStates/Edges/ContOtsu")
         
     if  path.exists("ImageStates/Edges/Kmeans") is False:
         os.mkdir("ImageStates/Edges/Kmeans")
+    else: 
+        shutil.rmtree("ImageStates/Edges/Kmeans", onerror=remove_readonly)
+        os.mkdir("ImageStates/Edges/Kmeans")
         
     if path.exists("ImageStates/Edges/Otsu") is False:
+        os.mkdir("ImageStates/Edges/Otsu")
+    else: 
+        shutil.rmtree("ImageStates/Edges/Otsu", onerror=remove_readonly)
         os.mkdir("ImageStates/Edges/Otsu")
         
     if path.exists("ImageStates/ImagePrep") is False:
@@ -54,8 +65,14 @@ def DirCheck():
         
     if path.exists("ImageStates/ImagePrep/Blurs") is False:
         os.mkdir("ImageStates/ImagePrep/Blurs")
+    else: 
+        shutil.rmtree("ImageStates/ImagePrep/Blurs", onerror=remove_readonly)
+        os.mkdir("ImageStates/ImagePrep/Blurs")
         
     if path.exists("ImageStates/ImagePrep/Sections") is False:
+        os.mkdir("ImageStates/ImagePrep/Sections")
+    else: 
+        shutil.rmtree("ImageStates/ImagePrep/Sections", onerror=remove_readonly)
         os.mkdir("ImageStates/ImagePrep/Sections")
         
     if path.exists("ImageStates/Thresholding") is False:
@@ -63,6 +80,18 @@ def DirCheck():
         
     if path.exists("ImageStates/Thresholding/Kmeans") is False:
         os.mkdir("ImageStates/Thresholding/Kmeans")
+    else: 
+        shutil.rmtree("ImageStates/Thresholding/Kmeans", onerror=remove_readonly)
+        os.mkdir("ImageStates/Thresholding/Kmeans")
         
     if path.exists("ImageStates/Thresholding/Otsu") is False:
         os.mkdir("ImageStates/Thresholding/Otsu")
+    else: 
+        shutil.rmtree("ImageStates/Thresholding/Otsu", onerror=remove_readonly)
+        os.mkdir("ImageStates/Thresholding/Otsu")
+        
+        
+def remove_readonly(func, path, excinfo):
+    os.chmod(path, stat.S_IWRITE)
+    func(path)
+        
